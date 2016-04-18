0e0ac0a5f4af3b0fd625dd08562660106e1a1ca6
radengoots <raden.goots@yahoo.com>
2016-04-18 20:36:29 +1000

    Working pull mode, on progress push mode

--- ass1-2016S1/new attempt/server.py
+++ ass1-2016S1/new attempt/server.py
@@ -1,40 +1,30 @@
+import Queue
+import select
 import socket
 import sys
-import select
-from thread import *
-import time
 
 RECV_BUFFER = 4096
 
 
-def load_ebook(book_name, page_number):
+def get_ebook(book_name, page_number):
     try:
         ebook_file = open(book_name + '_page' + page_number, 'r')
         ebook = ebook_file.read()
     except IOError:
         print('File not found')
-        return 'File not found'
+        return False
     else:
         ebook_file.close()
         return ebook
 
-def clientThread(conn):
-    # while True:
-    while True:
-        request = conn.recv(RECV_BUFFER)
-        print("Request: " + request)
-        request = request.split(' ')
-        # print(request)
-        if request[0] == 'display':
-            ebook = load_ebook(request[1], request[2])
-            conn.sendall(ebook)
-        else:
-            conn.sendall(str(request))
 
-        if request[0] == 'stop':
-            break
+def display(username, mode, book_name, page_number):
+    ebook = get_ebook(book_name, page_number)
 
-    conn.close()
+    if not ebook:
+        pass
+    else:
+        return 'File not found'
 
 
 def main():
@@ -42,12 +32,17 @@ def main():
     # TODO: check port availability
 
     connection_list = []
+    # [username, mode, polling_interval, ip, port]
+    connected_clients = []
+    message_queues = {}
+    outputs = []
 
     server_port = int(sys.argv[1])
 
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.setblocking(0)
         server_socket.bind(('', server_port))
         server_socket.listen(10)
     except socket.error, msg:
@@ -62,7 +57,8 @@ def main():
 
     while True:
         # get list sockets  which are ready to read through select
-        read_sockets, write_sockets, error_sockets = select.select(connection_list, [], [])
+        read_sockets, write_sockets, error_sockets = select.select(
+            connection_list, outputs, [])
 
         for sock in read_sockets:
             # New connection
@@ -70,12 +66,14 @@ def main():
                 print('ready to receive')
                 try:
                     conn, addr = server_socket.accept()
+                    conn.setblocking(0)
                 except socket.timeout:
                     print('Timed out waiting for a connection')
                     continue
 
                 connection_list.append(conn)
-                print "Client (%s, %s) connected" % addr
+                print "Client (%s, %s) is connected" % addr
+                message_queues[conn] = Queue.Queue()
 
             # Data coming from a client
             else:
@@ -83,14 +81,38 @@ def main():
                     data = sock.recv(RECV_BUFFER)
                     if data:
                         print data
-                        conn.sendall(data)
-                    # connection_list.append(server_socket)
+                        data = str(data).split(',')
+                        if data[0] == 'd':
+                            print(addr)
+                            conn.sendall(addr)
+                        elif data[0] == 's':
+                            # data[0]: code, [1]: username,[2]: mode, [3]: polling_interval
+                            connected_clients.append(
+                                [data[1], data[2], int(data[3]), addr[0],
+                                 addr[1]])
+                            print(
+                                data[1], data[2], int(data[3]), addr[0],
+                                addr[1])
+                            message_queues[sock].put('ok')
+                            if sock not in outputs:
+                                outputs.append(sock)
+                                # conn.sendall('ok')
+
                 # Client disconnected, so remove from socket list
                 except:
                     print "Client (%s, %s) is offline" % addr
                     sock.close()
                     connection_list.remove(sock)
                     continue
+
+        for sock in write_sockets:
+            try:
+                next_msg = message_queues[sock].get_nowait()
+
+            except Queue.Empty:
+                outputs.remove(sock)
+            else:
+                sock.send(next_msg)
 
     server_socket.close()
 
--- ass1-2016S1/server.py
+++ ass1-2016S1/server.py
@@ -1,45 +1,155 @@
 import socket
 import sys
+import uuid
 from thread import *
 
 RECV_BUFFER = 4096
+# dict with key ip:port_number and value: username, mode, polling_int
+clients = dict()
+# dict with key book_name_page_number
+# and value: [post_id, username, line_number, post]
+discussions = dict()
+# dict with key username and value: [posts_id]
+read_posts = dict()
+push_list = []
 
-def load_ebook(book_name, page_number):
+
+def get_post(book_name, page_number, post_id):
+    """
+    Get specific post based on post_id
+    :param book_name:
+    :param page_number:
+    :param post_id:
+    :return: (array) post
+    """
+    for post in discussions[book_name + page_number]:
+        if post[0] == post_id:
+            return ';'.join(post)
+    else:
+        return '0'
+
+
+def get_post_ids(book_name, page_number):
+    """
+    Get post ids associated with book_name + page_number
+    :param book_name:
+    :param page_number:
+    :return: (array) post ids
+    """
+    try:
+        posts = discussions[book_name + page_number]
+        post_ids = ''
+        for post in posts:
+            print post
+            post_ids += post[0] + ';'
+    except KeyError:
+        return '0'
+    else:
+        return post_ids[:-1]
+
+
+def get_ebook(book_name, page_number, id_, mode):
+    """
+    Get ebook from file
+    :param book_name: string
+    :param page_number: string
+    :param id_:
+    :param mode:
+    :return: (string) ebook
+    """
     try:
         ebook_file = open(book_name + '_page' + page_number, 'r')
         ebook = ebook_file.read()
     except IOError:
         print('File not found')
-        return 'File not found'
+        return False
     else:
         ebook_file.close()
         return ebook
 
 
-def clientThread(conn):
-    # while True:
-    while True:
-        request = conn.recv(RECV_BUFFER)
-        print("Request: " + request)
-        request = request.split(' ')
-        # print(request)
-        if request[0] == 'display':
-            ebook = load_ebook(request[1], request[2])
-            conn.sendall(ebook)
-        else:
-            conn.sendall(str(request))
+def client_thread(conn_, addr_):
+    """
+    Spawned thread for each client
+    :param conn_: client connection
+    :param addr_: client address
+    :return: none
+    """
 
-        if request[0] == 'stop':
+    # client_id = ip_address + port_number
+    client_id = addr_[0] + ':' + str(addr_[1])
+    while True:
+        data = conn_.recv(RECV_BUFFER)
+        print(client_id + ": " + data)
+
+        data = data.split(';')
+        request = data[0]
+
+        # Setup request in format: 's', username, mode, polling_interval
+        if request == 's':
+            clients.update({client_id: [data[1],
+                                        data[2],
+                                        int(data[3])]})
+            conn_.sendall('ok')
+            if data[2] == 'push':
+                listener_port = int(conn_.recv(RECV_BUFFER))
+                push_list.append([addr_[0], listener_port])
+                print(push_list)
+        # Display request in format: 'd', username, book_name, page_number
+        elif request == 'd':
+            ebook = get_ebook(data[1], data[2], client_id,
+                              clients[client_id][1])
+            if not ebook:
+                conn_.sendall('File not found')
+            else:
+                conn_.sendall(ebook)
+        # Check post request in format: 'c', book_name, page_number
+        elif request == 'c':
+            post_ids = get_post_ids(data[1], data[2])
+            conn_.sendall(post_ids)
+        # Post to discussion in format: 'p', book_name, page_number,
+        #                               line_number, content_of_post
+        elif request == 'p':
+            username = clients[client_id][0]
+            post_id = str(uuid.uuid4())[:8]
+            if get_ebook(data[1], data[2], client_id,
+                         username[1]):
+                if data[1] + data[2] in discussions:
+                    discussions[data[1] + data[2]].append(
+                        [post_id, username, data[3],
+                         data[4]])
+                else:
+                    discussions[data[1] + data[2]] = [[
+                        post_id,
+                        username,
+                        data[3],
+                        data[4]]]
+                print(discussions)
+                conn_.sendall('Post #' + post_id + ' saved')
+
+        # Get post request in format: 'g', book_name, page_number, post_id
+        elif request == 'g':
+            post = get_post(data[1], data[2], data[3])
+            if post:
+                conn_.sendall(post)
+            else:
+                conn_.sendall('Post not found')
+        else:
+            conn_.sendall(str(data))
+
+        if request == 'q':
             break
 
-    conn.close()
+    print(client_id + ' is disconnected')
+    conn_.close()
 
 
-def main():
-    # TODO: validate number of arguments
-    # TODO: check port availability
-
+if __name__ == '__main__':
     server_port = int(sys.argv[1])
+
+    if server_port < 1024:
+        print('Reserved port, please pick another port number.')
+        sys.exit()
 
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,43 +163,16 @@ def main():
     print("The server is listening on port number " + str(server_port))
     print("The database for discussion posts has been initialised")
 
-    # conn, addr = server_socket.accept()
     while True:
         try:
-            print('ready to receive')
+            print('Ready to receive')
             conn, addr = server_socket.accept()
         except socket.timeout:
             print('Timed out waiting for a connection')
             continue
 
-        print('Got request from ' + addr[0] + ': ' + str(addr[1]))
+        print(addr[0] + ': ' + str(addr[1]) + ' is connected')
 
-        start_new_thread(clientThread, (conn,))
-        # # while True:
-        # request = conn.recv(RECV_BUFFER)
-        # print("Request: " + request)
-        # request = request.split(' ')
-        # # print(request)
-        # if request[0] == 'display':
-        #     ebook = load_ebook(request[1], request[2])
-        #     conn.sendall(ebook)
-        # if request[0] == 'stop':
-        #     print(request)
-        #     # conn.close()
-        #     break
-
-        # print("Request: " + request)
-        # if not request:
-        #     break
-        # request = request.split(' ')
-        # # print(request)
-        # if request[0] == 'display':
-        #     ebook = load_ebook(request[1], request[2])
-        #     conn.sendall(ebook)
-        # conn.close()
+        start_new_thread(client_thread, (conn, addr))
 
     server_socket.close()
-
-
-if __name__ == '__main__':
-    main()
